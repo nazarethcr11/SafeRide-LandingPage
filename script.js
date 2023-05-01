3e9eda79a5907d62ff8a8049c9f1d1878585f82d
Silene Nazareth Casma Rubio <85361356+nazarethcr11@users.noreply.github.com>
2023-05-01 10:46:19 -0500

    Add: Complete project

---
+++ script.js
@@ -0,0 +1,37 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 580);
+});
+
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.nav-list');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll =()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+const sr= ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2500,
+    reset:true
+});
+
+sr.reveal('.home-text',{delay:300});
+sr.reveal('.home-img',{delay:400});
+sr.reveal('.container',{delay:400});
+
+sr.reveal('.about-img',{});
+sr.reveal('.about-text',{delay:300});
+
+sr.reveal('.middle-text',{});
+sr.reveal('.row-btn, .app-content',{delay:300});
+
+sr.reveal('.review-content, .contact',{delay:300});
