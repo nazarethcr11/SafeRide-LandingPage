467c832f4fc8fc479bbdbe43bc5a6a3eb5e29f30
Caleb Reategui <104101833+caleb210603@users.noreply.github.com>
2023-05-03 17:19:10 -0500

    Update: Animations

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ const sr= ScrollReveal({
     reset:true
 });
 
-sr.reveal('.home-text',{delay:300});
-sr.reveal('.home-img',{delay:400});
-sr.reveal('.container',{delay:400});
+sr.reveal('.home-text',{delay:200});
+sr.reveal('.home-img',{delay:300});
+sr.reveal('.container',{delay:300});
 
 sr.reveal('.about-img',{});
-sr.reveal('.about-text',{delay:300});
+sr.reveal('.about-text',{delay:200});
 
 sr.reveal('.middle-text',{});
-sr.reveal('.row-btn, .app-content',{delay:300});
+sr.reveal('.row-btn, .app-content',{delay:200});
 
-sr.reveal('.review-content, .contact',{delay:300});
+sr.reveal('.review-content, .contact',{delay:200});
